1da37184e5602dcc817686c8e1c56a4b0c980d0f
Lovely Sharma <lovelysingeras297@gmail.com>
2023-11-18 23:02:45 +0530

    Added docs for Typography

---
+++ Material UI/Demo/src/components/Typography/MuiTypography.jsx
@@ -0,0 +1,32 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <>
+      <Typography variant="h1">Heading 1</Typography>
+      <Typography variant="h2">Heading 2</Typography>
+      <Typography variant="h3" gutterBottom>Heading 3</Typography>
+      <Typography variant="h4" gutterBottom>Heading 4</Typography>
+      <Typography variant="h5">Heading 5</Typography>
+      <Typography variant="h6">Heading 6</Typography>
+
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur.
+      </Typography>
+    </>
+  );
+};
